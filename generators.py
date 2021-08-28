43125e9dc0cb194c9fafa9eb262876a7e183626a
cristianiniguez <felizpc1@gmail.com>
2021-08-28 15:50:04 -0400

    estructuras de datos avanzadas: mejorando nuestra sucesion de fibonacci - reto

--- generators.py
+++ generators.py
@@ -1,25 +1,27 @@
 import time
 
 
-def fibo_gen():
+def fibo_gen(max=None):
     n1 = 0
     n2 = 1
     counter = 0
     while True:
         if counter == 0:
-            counter += 1
-            yield n1
+            result = n1
         elif counter == 1:
-            counter += 1
-            yield n2
+            result = n2
         else:
             n1, n2 = n2, n1 + n2
-            counter += 1
-            yield n2
+            result = n2
+        counter += 1
+        if not max or result <= max:
+            yield result
+        else:
+            break
 
 
 if __name__ == '__main__':
-    fibonacci = fibo_gen()
+    fibonacci = fibo_gen(10)
     for element in fibonacci:
         print(element)
         time.sleep(1)
