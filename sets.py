62132d1100671c3d906457f281e25e2da4f72dab
cristianiniguez <felizpc1@gmail.com>
2021-08-28 16:36:47 -0400

    estructuras de datos avanzadas: eliminando los repetidos de una lista - reto

--- sets.py
+++ sets.py
@@ -1,9 +1,5 @@
-def remove_duplicates(list):
-    new_list = []
-    for item in list:
-        if item not in new_list:
-            new_list.append(item)
-    return new_list
+def remove_duplicates(my_list):
+    return list(set(my_list))
 
 
 def run():
