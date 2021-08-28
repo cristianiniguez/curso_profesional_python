e5270542cc0922925b39bcff99b193945d5f7f30
cristianiniguez <felizpc1@gmail.com>
2021-08-28 15:10:28 -0400

    estructuras de datos avanzadas: la sucesion de fibonacci - reto

--- iterators.py
+++ iterators.py
@@ -2,6 +2,9 @@ import time
 
 
 class FiboIter():
+    def __init__(self, max=None):
+        self.max = max
+
     def __iter__(self):
         self.n1 = 0
         self.n2 = 1
@@ -10,19 +13,22 @@ class FiboIter():
 
     def __next__(self):
         if self.counter == 0:
-            self.counter += 1
-            return self.n1
+            result = self.n1
         elif self.counter == 1:
-            self.counter += 1
-            return self.n2
+            result = self.n2
         else:
             self.n1, self.n2 = self.n2, self.n1 + self.n2
+            result = self.n2
+
+        if not self.max or result <= self.max:
             self.counter += 1
-            return self.n2
+            return result
+        else:
+            raise StopIteration
 
 
 if __name__ == "__main__":
-    fibonacci = FiboIter()
+    fibonacci = FiboIter(10)
     for element in fibonacci:
         print(element)
         time.sleep(1)
