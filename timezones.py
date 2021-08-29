ce6ecaab687d92e0c046cc6d8007e1fddb11a908
cristianiniguez <felizpc1@gmail.com>
2021-08-29 16:05:08 -0400

    bonus: timezones

---
+++ timezones.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import pytz
+
+bogota_timezone = pytz.timezone('America/Bogota')
+bogota_datetime = datetime.now(bogota_timezone)
+print("Bogotá: ", bogota_datetime.strftime('%d/%m/%Y, %H:%M:%S'))
+
+mexico_timezone = pytz.timezone('America/Mexico_City')
+mexico_datetime = datetime.now(mexico_timezone)
+print("Ciudad de México: ", mexico_datetime.strftime('%d/%m/%Y, %H:%M:%S'))
+
+caracas_timezone = pytz.timezone('America/Caracas')
+caracas_datetime = datetime.now(caracas_timezone)
+print("Caracas: ", caracas_datetime.strftime('%d/%m/%Y, %H:%M:%S'))
